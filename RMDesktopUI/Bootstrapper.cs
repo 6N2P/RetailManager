b9ab6a95db2c08aa072243163fe593d90cfd9490
sergey <ivanovsv@tmnkonst.ru>
2023-01-19 15:43:09 +0500

    Добавил в проект WPF, начал настраивать.

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    /// <summary>
+    /// Класс для настройки калибра и микро
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
